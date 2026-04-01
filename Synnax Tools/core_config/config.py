8bb52024fa4e56a9a62746fe34613348fd423d25
brettp13 <bpaden@purdue.edu>
2026-03-31 23:38:37 -0400

    device + channel config

--- Synnax Tools/core_config/config.py
+++ Synnax Tools/core_config/config.py
@@ -1,3 +1,4 @@
+import csv
 import synnax as sy
 from synnax import ni
 
@@ -10,6 +11,43 @@ rack = client.racks.retrieve_embedded_rack()
 devices = []
 device_map = {} # Initialize from config file
 
+rows = []
+device_rows = False
+
+with open('pigtails.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == "cRIO Slot":
+            device_rows = True
+            continue;
+        if device_rows:
+            if row[0] == '':
+                break
+
+            rows.append(row)
+
+
+for row in rows:
+    modbus = row[0]
+    card_type = row[1]
+
+    card_num = 1
+
+    while True:
+        if device_map[card_type + "-" + str(card_num)]:
+            card_num += 1
+        else:
+            name = card_type + "-" + str(card_num)
+            device_map[name] = ni.Device(
+                                identifier="dev_mod" + str(modbus),
+                                name=name,
+                                model="NI " + card_type,
+                                location="cDAQ1/dev_mod" + str(modbus),
+                                rack=rack.key,
+                            )
+            
+
+"""
 # Configure device using the ni.Device class - each module is its own device
 devices.append(ni.Device(
     identifier="dev_mod1",
@@ -67,9 +105,10 @@ devices.append(ni.Device(
     location="cDAQ1/dev_mod1",
     rack=rack.key,
 ))
+"""
 
 # Create the devices in Synnax
-for device in devices:
+for device in device_map:
     device = client.devices.create(device)
 
 # # Or retrieve by model
@@ -78,5 +117,37 @@ for device in devices:
 # # Or retrieve by name
 # device = client.devices.retrieve(name="My NI Module")
 
-for device in devices:
+for device in device_map:
     print(f"Device configured: {device.name} (key={device.key})")
+
+rows = []
+channel_rows = False
+
+channels = {}
+
+channels["ai_time"] = client.channels.create(
+    name="ai_time",
+    is_index=True,
+    data_type=sy.DataType.TIMESTAMP,
+    retrieve_if_name_exists=True,
+)
+
+with open('pigtails.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == "P&ID Name":
+            channel_rows = True
+            continue;
+        if channel_rows:
+            rows.append(row)
+
+
+for row in rows:
+    channels[row[0]] = client.channels.create(
+        name=row[0],
+        index=channels["ai_time"].key,
+        data_type=sy.DataType.FLOAT32,
+        retrieve_if_name_exists=True,
+    )
+
+print(channels);
